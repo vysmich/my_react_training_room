2fe034f3b89b3d6fd7a1e695d2dec34f59efd94a
[vysmich] <[vysmich@gmail.com]>
2021-06-01 14:42:59 +0200

    instal scss, deleted react logo, added normalize and edited App.js

---
+++ src/EntranceGate.js
@@ -0,0 +1,16 @@
+import './EntranceGate.scss';
+
+function EntranceGate() {
+  return (
+<div className="EntranceGate">
+<header>
+  <h1>Hello, I am Michal</h1>
+  <p> and this is my React training room</p>
+  <button>Enter</button>
+</header>
+</div>
+
+  );
+}
+
+export default EntranceGate;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import EntranceGate from './EntranceGate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EntranceGate />
   </React.StrictMode>,
   document.getElementById('root')
 );
