2f3a89679d8bf580beeed1462df9b39c5d2e6ae8
[vysmich] <[vysmich@gmail.com]>
2021-06-26 08:14:46 +0200

    added style for HomePage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,81 +21,79 @@ import "./NavBar.scss";
 const NavBar = () => {
   return (
     <Router>
-      <div>
-        <nav className="nav-bar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/CalculatorApp">Calculator</Link>
-            </li>
-            <li>
-              <Link to="/WeatherApp">Actual weather</Link>
-            </li>
-            <li>
-              <Link to="/ToDoApp">To do</Link>
-            </li>
-            <li>
-              <Link to="/CryptoApp">Cryptocurrency rates</Link>
-            </li>
-            <li>
-              <Link to="/SpotifyApp">Spotify</Link>
-            </li>
-            <li>
-              <Link to="/SocialApp">Social Network</Link>
-            </li>
-            <li>
-              <Link to="/EshopApp">Eshop</Link>
-            </li>
-            <li>
-              <Link to="/DasboardApp">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/ChatApp">Chat</Link>
-            </li>
-            <li>
-              <Link to="/BlogApp">Blog</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav className="nav-bar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/CalculatorApp">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/WeatherApp">Actual weather</Link>
+          </li>
+          <li>
+            <Link to="/ToDoApp">To do</Link>
+          </li>
+          <li>
+            <Link to="/CryptoApp">Cryptocurrency rates</Link>
+          </li>
+          <li>
+            <Link to="/SpotifyApp">Spotify</Link>
+          </li>
+          <li>
+            <Link to="/SocialApp">Social Network</Link>
+          </li>
+          <li>
+            <Link to="/EshopApp">Eshop</Link>
+          </li>
+          <li>
+            <Link to="/DasboardApp">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/ChatApp">Chat</Link>
+          </li>
+          <li>
+            <Link to="/BlogApp">Blog</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/ToDoApp">
-            <ToDoApp />
-          </Route>
-          <Route path="/CalculatorApp">
-            <CalculatorApp />
-          </Route>
-          <Route path="/WeatherApp">
-            <WeatherApp />
-          </Route>
-          <Route path="/CryptoApp">
-            <CryptoApp />
-          </Route>
-          <Route path="/SpotifyApp">
-            <SpotifyApp />
-          </Route>
-          <Route path="/SocialApp">
-            <SocialApp />
-          </Route>
-          <Route path="/EshopApp">
-            <EshopApp />
-          </Route>
-          <Route path="/DasboardApp">
-            <DasboardApp />
-          </Route>
-          <Route path="/ChatApp">
-            <ChatApp />
-          </Route>
-          <Route path="/BlogApp">
-            <BlogApp />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/ToDoApp">
+          <ToDoApp />
+        </Route>
+        <Route path="/CalculatorApp">
+          <CalculatorApp />
+        </Route>
+        <Route path="/WeatherApp">
+          <WeatherApp />
+        </Route>
+        <Route path="/CryptoApp">
+          <CryptoApp />
+        </Route>
+        <Route path="/SpotifyApp">
+          <SpotifyApp />
+        </Route>
+        <Route path="/SocialApp">
+          <SocialApp />
+        </Route>
+        <Route path="/EshopApp">
+          <EshopApp />
+        </Route>
+        <Route path="/DasboardApp">
+          <DasboardApp />
+        </Route>
+        <Route path="/ChatApp">
+          <ChatApp />
+        </Route>
+        <Route path="/BlogApp">
+          <BlogApp />
+        </Route>
+      </Switch>
     </Router>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,30 @@
 import React from "react";
-
+import Hero from "../img/gif.gif";
+import GitHub from "../img/icons8-github-64.png";
+import LinkedIn from "../img/icons8-linkedin-64.png";
+import Gmail from "../img/Gmail-Logo-700x394.png";
+import "./HomePage.scss";
 const HomePage = () => {
   return (
-    <div className="container">
-      <h1>Home</h1>
-      <p>This page was created for purpose to increase my react skill</p>
-    </div>
+    <main>
+      <div className="container home-page">
+        <h1>Hello, I'm Michal Vyskočil</h1>
+        <p>I am a junior frontend developer/ coder</p>
+        <p>and this is my training room to improve my React.js skills</p>
+        <div className="contacts">
+          <a href="https://github.com/vysmich">
+            <img src={GitHub} alt="Github logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/vysmich/">
+            <img src={LinkedIn} alt="LinkedIn logo" />
+          </a>
+          <a href="mailto:vysmich@gmail.com">
+            <img src={Gmail} alt="LinkedIn logo" />
+          </a>
+        </div>
+        <img src={Hero} alt="" className="hero" />
+      </div>
+    </main>
   );
 };
 
