835c7ccdaedbd9189b479da17d58eb192cd919b5
[vysmich] <[vysmich@gmail.com]>
2021-07-05 09:52:29 +0200

    data filtering improvements

--- src/pages/WeatherApp.js
+++ src/pages/WeatherApp.js
@@ -7,49 +7,45 @@ import ForecastBox from "../components/WeatherApp/ForecastBox/ForecastBox";
 
 const WeatherApp = () => {
   //State
+  const [actualWeather, setactualWeather] = useState({});
   const [forecasts, setForecasts] = useState({});
 
-  const filterData = ({ location, current, forecast }) => {
+  // filter for get actual weather from API
+  const filterActualData = ({ location, current }) => {
     return {
       location: location.name,
       currentTemp: current.temp_c,
       currentWind: current.wind_kph,
       forecastImg: current.condition.icon,
-      today: {
-        date: forecast.forecastday[0].date,
-        icon: forecast.forecastday[0].day.condition.icon,
-        maxTemp: forecast.forecastday[0].day.maxtemp_c,
-        minTemp: forecast.forecastday[0].day.mintemp_c,
-        chanceOfRain: forecast.forecastday[0].day.daily_chance_of_rain,
-        maxWind: forecast.forecastday[0].day.maxwind_kph,
-      },
-      tomorrow: {
-        date: forecast.forecastday[1].date,
-        icon: forecast.forecastday[1].day.condition.icon,
-        maxTemp: forecast.forecastday[1].day.maxtemp_c,
-        minTemp: forecast.forecastday[1].day.mintemp_c,
-        chanceOfRain: forecast.forecastday[1].day.daily_chance_of_rain,
-        maxWind: forecast.forecastday[1].day.maxwind_kph,
-      },
-      afterTomorrow: {
-        date: forecast.forecastday[2].date,
-        icon: forecast.forecastday[2].day.condition.icon,
-        maxTemp: forecast.forecastday[2].day.maxtemp_c,
-        minTemp: forecast.forecastday[2].day.mintemp_c,
-        chanceOfRain: forecast.forecastday[2].day.daily_chance_of_rain,
-        maxWind: forecast.forecastday[2].day.maxwind_kph,
-      },
     };
+  };
+
+  // filter for get forecast from API
+  const filterForecastData = ({ forecast }) => {
+    let daysforecast = [];
+    forecast.forecastday.map((days) => {
+      daysforecast.push({
+        date: days.date,
+        icon: days.day.condition.icon,
+        maxTemp: days.day.maxtemp_c,
+        minTemp: days.day.mintemp_c,
+        chanceOfRain: days.day.daily_chance_of_rain,
+        maxWind: days.day.maxwind_kph,
+      });
+    });
+    return daysforecast;
   };
 
   // function for api request and set state
   const handleSearch = (query) => {
     fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=0a7c1ff2372040d5829153426213006&q=${query}&days=5&aqi=no&alerts=no`
+      `http://api.weatherapi.com/v1/forecast.json?key=0a7c1ff2372040d5829153426213006&q=${query}&days=4&aqi=no&alerts=no`
     )
       .then((response) => response.json())
       .then((data) => {
-        let forecastData = filterData(data);
+        let actualData = filterActualData(data);
+        let forecastData = filterForecastData(data);
+        setactualWeather(actualData);
         setForecasts(forecastData);
       });
   };
