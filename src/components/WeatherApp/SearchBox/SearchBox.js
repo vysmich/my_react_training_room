413a7db25483b3504fa39f13f49ecd21b1651e9c
[vysmich] <[vysmich@gmail.com]>
2021-07-07 11:26:48 +0200

    remove diacritics from searchBox

--- src/components/WeatherApp/SearchBox/SearchBox.js
+++ src/components/WeatherApp/SearchBox/SearchBox.js
@@ -1,12 +1,13 @@
 import React, { useRef } from "react";
 import debounce from "lodash.debounce";
+import latinize from "latinize";
 import "./SearchBox.scss";
 
 const SearchBox = (props) => {
   const searchInput = useRef();
 
   const searchForecast = () => {
-    props.onSearch(searchInput.current.value);
+    props.onSearch(latinize(searchInput.current.value));
   };
 
   const handleChange = debounce(searchForecast, 1200);
