fe9a9c6b32ae36eeb128696aabe4197d54f49dea
[vysmich] <[vysmich@gmail.com]>
2021-07-29 14:46:09 +0200

    added disappearance of carousel arrows

--- src/components/WeatherApp/ForecastBox/HourlyForecast/HourlyCarusel/HourlyCarusel.js
+++ src/components/WeatherApp/ForecastBox/HourlyForecast/HourlyCarusel/HourlyCarusel.js
@@ -3,14 +3,28 @@ import "./HourlyCarusel.scss";
 import CarouselItem from "./CarouselItem/CarouselItem";
 
 const HourlyCarusel = (props) => {
+  // Hourly forecast carousel state
+  const [activeIndex, setactiveIndex] = useState(0);
+  //Forecast data
   const forecast = props.forecast;
+  const btnRight = document.querySelector(".btn-right");
+
   // Filters the forecast data by pressing the button in DailyForecast component
   const hourlyData = forecast.filter(
     (date) => date.day === props.showHourlyForecast[1]
   )[0].hourly;
+  // function for check if is element in viewport
+  const isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.right >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 
-  // Hourly forecast carousel state
-  const [activeIndex, setactiveIndex] = useState(0);
   return (
     <div className="carousel">
       <div
@@ -18,29 +32,42 @@ const HourlyCarusel = (props) => {
         style={{ transform: `translateX(-${activeIndex * 100}%)` }}
       >
         {hourlyData.map((hour) => {
-          return <CarouselItem hour={hour} />;
+          return <CarouselItem key={hour.time} hour={hour} />;
         })}
       </div>
 
       <div className="indicators">
-        <button
-          className="btn-left"
-          onClick={() => {
-            activeIndex <= 0.3
-              ? setactiveIndex(0)
-              : setactiveIndex(activeIndex - 0.3);
-          }}
-        >
-          &lt;
-        </button>
-        <button
-          className="btn-right"
-          onClick={() => {
-            setactiveIndex(activeIndex + 0.3);
-          }}
-        >
-          &gt;
-        </button>
+        {activeIndex > 0 && (
+          <button
+            className="btn-left"
+            onClick={() => {
+              btnRight.style = "display:block";
+              activeIndex <= 0.3
+                ? setactiveIndex(0)
+                : setactiveIndex(activeIndex - 0.3);
+            }}
+          >
+            &lt;
+          </button>
+        )}
+        {
+          <button
+            className="btn-right"
+            onClick={() => {
+              //last item in carousel
+              var lastCarouselItem = document.querySelector(
+                ".carousel-item:last-child"
+              );
+              if (!isInViewport(lastCarouselItem)) {
+                setactiveIndex(activeIndex + 0.3);
+              } else {
+                btnRight.style = "display:none";
+              }
+            }}
+          >
+            &gt;
+          </button>
+        }
       </div>
     </div>
   );
